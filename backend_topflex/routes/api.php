8b3cb31b6d85855ff62ca19e800961721548e025
Francucho2309 <franco.ramos@cirvianum.cat>
2026-01-06 00:26:44 +0100

    Añadido CRUD ligas

--- backend_topflex/routes/api.php
+++ backend_topflex/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\LeagueController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,4 +19,15 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
     Route::delete('/users/{user}', [UserController::class, 'destroy']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/leagues', [LeagueController::class, 'index']);
+    Route::get('/leagues/{league}', [LeagueController::class, 'show']);
+});
+
+Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+    Route::post('/leagues', [LeagueController::class, 'store']);
+    Route::put('/leagues/{league}', [LeagueController::class, 'update']);
+    Route::delete('/leagues/{league}', [LeagueController::class, 'destroy']);
 });
