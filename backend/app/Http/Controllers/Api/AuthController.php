94e588e185d77a5108424168eb201010523aac52
Francucho2309 <franco.ramos@cirvianum.cat>
2026-03-09 21:52:57 +0100

    Actualización 09/03 - 3.2

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6'
+            'password' => 'required|confirmed|min:6'
         ]);
 
         $user = User::create([
